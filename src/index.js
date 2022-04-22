4d27b4fc6a692fdc2af06bfb2438c5a87ee41f16
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-22 09:59:31 -0700

    build out UI basics for testing

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import $ from 'jquery';
+// import CurrencyExchange from './js/currency-api.js';
+
+// function displayExchange(amount) {
+//   return;
+// }
+
+// $(document).ready(function() {
+//   let promise = CurrencyExchange.getValue();
+//   promise.then(function(response) {
+//     const body = JSON.parse(response);
+//     let userPrice = body[0].
+//   })
+// })
