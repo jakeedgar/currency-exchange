770217472f480e91c958259bebd160898365ef12
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-22 13:52:52 -0700

    WIP update UI and JS logic

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './css/styles.css';
 import $ from 'jquery';
 import CurrencyExchange from './js/currency-api.js';
 
-// function displayExchange(amount) {
-//   return;
-// }
+function displayExchange(response) {
+  console.log(response);
+  console.log(response.target_code);
+  console.log(response.conversion_result);
+  if (response.main) {
+    $("#exchange-display").text(`Your ${CurrencyExchange.amount} in USD converts to: ${parseFloat(response.conversion_result).toFixed(2)} in ${response.target_code}`);
+  } else {
+    $('.show-errors').text(`${CurrencyExchange.error}`);
+  }
 
-function displayErrors(error) {
-  $('.show-errors').text(`${error}`);
 }
 
 $(document).ready(function() {
@@ -17,16 +21,9 @@ $(document).ready(function() {
     e.preventDefault();
     const userInput = $("#currency").val();
     const amount = $("#amount").val();
-    let promise = CurrencyExchange.getValue(userInput, amount);
-    promise.then(function(response) {
-      if (resonse instanceof Error) {
-        throw Error(`CurrencyExchange API error: ${response.message}`)
-      }
-      const body = JSON.parse(response);
-      $("#exchange-display").html(`Your ${amount} in USD converts to: ${parseFloat(body.conversion_result).toFixed(2)} in ${body.target_code}`);
-      promise.catch(error) {
-        displayErrors(error.message);
-      }
-    });
+    CurrencyExchange.getValue(userInput, amount)
+      .then(function(response) {
+        displayExchange(response);
+      });
   });
 });
